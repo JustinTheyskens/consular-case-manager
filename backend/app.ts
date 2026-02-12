05929c03e4cc44abfb980bea3d75b62da8b8b0f2
Wilson Kong <wkong@skillstorm.com>
2026-02-12 16:58:57 -0500

    Created express server and application

---
+++ backend/app.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import cors from "cors";
+import "dotenv/config";
+
+import logger from "./middleware/logger.ts";
+
+const app = express();
+const allowedOrigins = process.env.WHITELIST;
+
+app.use(cors({
+    origin: allowedOrigins
+}));
+
+app.use(express.json());
+
+app.use(logger);
+
+export default app;
