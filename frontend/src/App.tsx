017242936eff0853ab9a661292be555ee522fc50
JustinTheyskens <justin.theyskens@stonybrook.edu>
2026-02-20 17:19:37 -0500

    Home Layout Working

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,57 +1,62 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import CssBaseline from "@mui/material/CssBaseline";
 import { StaffDashboard } from "./layout/StaffDashboardLayout";
-import UserLoginPage from './pages/UserLoginPage';
-import StaffLoginPage from './pages/StaffLoginPage';
-
+import UserLoginPage from "./pages/UserLoginPage";
+import StaffLoginPage from "./pages/StaffLoginPage";
+import { Box } from "@mui/material";
 
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from "@emotion/react";
+import { theme } from "./theme";
+import { PageHeader } from "./components/PageHeader";
 
 const HomeLayout = () => {
     return (
-        <div>
-            <h1>Consular Case Manager</h1>
-            <Outlet />
-        </div>
+        <ThemeProvider theme={theme}>
+            <Box minHeight="100vh">
+                <PageHeader
+                    title="Consular Case Manager"
+                    subtitle="Passport appointments and case tracking"
+                />
+            </Box>
+            
+        </ThemeProvider>
     );
 };
-
-
-
 
 export default function App() {
     return (
         <>
-        {/* <StaffDashboard/> */}
-        <BrowserRouter>
-            <CssBaseline />
-            <Routes>
-                <Route
-                    path="/"
-                    element={<HomeLayout />}
-                >
+            {/* <StaffDashboard/> */}
+            <BrowserRouter>
+                <CssBaseline />
+                <Routes>
                     <Route
-                        path="user/login"
-                        element={<UserLoginPage />}
-                    />
-                    <Route
-                        path="user/dashboard"
-                        element={<div className="TODO--USER-DASHBOARD">User Dashboard</div>}
-                    />
+                        path="/"
+                        element={<HomeLayout />}
+                    >
+                        <Route
+                            path="user/login"
+                            element={<UserLoginPage />}
+                        />
+                        <Route
+                            path="user/dashboard"
+                            element={<div className="TODO--USER-DASHBOARD">User Dashboard</div>}
+                        />
 
-                    <Route
-                        path="staff/login"
-                        element={<StaffLoginPage />}
-                    />
-                    <Route
-                        path="staff/dashboard"
-                        element={<StaffDashboard />}
-                    />
+                        <Route
+                            path="staff/login"
+                            element={<StaffLoginPage />}
+                        />
+                        <Route
+                            path="staff/dashboard"
+                            element={<StaffDashboard />}
+                        />
 
-                    <Route path="dashboard"></Route>
-                </Route>
-            </Routes>
-        </BrowserRouter>
+                        <Route path="dashboard"></Route>
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
