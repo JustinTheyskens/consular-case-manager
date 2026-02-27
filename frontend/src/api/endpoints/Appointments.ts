b9975b827c14569557a9f56a91fb40eb196ec2f9
Nicholas Eason <nicholas.eason50@gmail.com>
2026-02-27 10:47:42 -0600

    Created RTK routes for Appointments and Cases

---
+++ frontend/src/api/endpoints/Appointments.ts
@@ -0,0 +1,31 @@
+import { baseApi } from "../BaseAPI.ts";
+
+export type Appointment = {
+    _id: string;
+    reference: number;
+    status: string;
+    appointment: Appointment;
+    assignedStaff: string;
+    citizen: string;
+    checkedIn: boolean;
+    flagged: boolean;
+    notes?: string;
+};
+
+export type UpdateAppointmentRequest = Partial<Omit<Appointment, "_id">> & { _id: string };
+
+export const casesApi = baseApi.injectEndpoints({
+    endpoints: (builder) => ({
+        // PUT /appointments/:id
+        updateAppointment: builder.mutation<Appointment, UpdateAppointmentRequest>({
+            query: ({ _id, ...body }) => ({ url: `/cases/${_id}`, method: "PUT", body }),
+            invalidatesTags: (result, error, arg) => [
+                { type: "Appointment" as const, id: arg["_id"] },
+                { type: "Appointment" as const, id: "LIST" },
+            ],
+        }),
+    }),
+    overrideExisting: false,
+});
+
+export const { useUpdateAppointmentMutation } = casesApi;
