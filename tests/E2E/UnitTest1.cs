dd4663a30c368cf8bac6a1d362215f1b146f4bfc
JustinTheyskens <justin.theyskens@stonybrook.edu>
2026-03-02 18:56:19 -0500

    added smoke tests

--- tests/E2E/UnitTest1.cs
+++ tests/E2E/UnitTest1.cs
@@ -5,23 +5,50 @@ using OpenQA.Selenium;
 
 public class ExamplePageTests
 {
-    private WebDriver _driver;
+    private static string BaseUrl = "http://localhost:5173";
+    private WebDriver driver;
+
     [SetUp]
     public void Setup()
     {
-        _driver = new ChromeDriver();
+        driver = new ChromeDriver();
+        driver.Manage().Window.Maximize();
     }
 
     [TearDown]
-    public void Teardown()
+    public void TearDown()
     {
-        _driver.Close();
+        //driver.Quit();
+        driver.Dispose();
     }
 
     [Test]
     public void Test()
     {
         Assert.Pass();
+    }
+
+    [Test]
+    public void HomePage_LoadsSuccessfully()
+    {
+        driver.Navigate().GoToUrl(BaseUrl);
+
+        var header = driver.FindElement(By.TagName("h1"));
+        Assert.That(header.Text, Is.Not.Empty);
+
+        //Assert.That(driver.FindElement(By.Id("page-header")).Displayed);
+    }
+
+    [Test]
+    public void StaffUser_Can_Login_Successfully()
+    {
+        driver.Navigate().GoToUrl(BaseUrl + "/staff/login");
+
+        driver.FindElement(By.Id("email")).SendKeys("staff@test.com");
+        driver.FindElement(By.Id("password")).SendKeys("Password123!");
+        driver.FindElement(By.CssSelector("button[type='submit']")).Click();
+
+        Assert.That(driver.Url, Does.Contain("/dashboard"));
     }
 
 }
