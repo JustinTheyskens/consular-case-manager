e557c8b201cb1770cabe6000f16b66689322e4db
Wilson Kong <wkong@skillstorm.com>
2026-03-04 16:57:14 -0500

    Undo backend changes

--- backend/models/appointments.model.ts
+++ backend/models/appointments.model.ts
@@ -7,21 +7,11 @@ export const appointmentTypes = [
     "passport-lost",
 ] as const;
 
-export const appointmentStatuses = [
-    "scheduled",
-    "in-review",
-    "approved",
-    "rejected",
-    "completed",
-] as const;
-
 export type AppointmentType = (typeof appointmentTypes)[number];
-export type AppointmentStatus = (typeof appointmentStatuses)[number];
 
 export interface IAppointment extends mongoose.Document {
     type: AppointmentType;
     time: Date;
-    status: AppointmentStatus;
 }
 
 const appointmentSchema = new Schema<IAppointment>({
@@ -34,13 +24,6 @@ const appointmentSchema = new Schema<IAppointment>({
     time: {
         required: true,
         type: Date,
-    },
-
-    status: {
-        required: true,
-        type: String,
-        enum: appointmentStatuses,
-        default: "scheduled",
     },
 });
 
