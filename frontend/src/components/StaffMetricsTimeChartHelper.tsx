1fe8bb07a96c6b34d88893f8334642ff3694bfec
Gavin Scheidler <GavinMSScheidler@gmail.com>
2026-03-04 16:59:00 -0600

    Updated Dash & Filter
    Updated the metrics dashboard to have a filter on it, also added counts and an accordion

---
+++ frontend/src/components/StaffMetricsTimeChartHelper.tsx
@@ -0,0 +1,37 @@
+function lastNumDays(num: number) {
+  const today = new Date();
+  const lastNDays: Record<string, number> = {};
+  if (num >= 0) {
+    for (let i = num; i >= 0; i--) {
+      const d = new Date();
+      d.setDate(today.getDate() - i);
+      const key = d.toISOString().split("T")[0];
+      lastNDays[key] = 0;
+    }
+  } else {
+    for (let i = num; i <= 0; i++) {
+      const d = new Date();
+      d.setDate(today.getDate() - i);
+      const key = d.toISOString().split("T")[0];
+      lastNDays[key] = 0;
+    }
+  }
+  return lastNDays;
+}
+
+function createRangeOptions() {
+  const rangeOptions = [];
+
+  for (let i = -60; i < 60; i += 15) {
+    if (i != 0) {
+      rangeOptions.push({
+        start: i,
+        end: i + 15,
+        label: `${i < 0 ? Math.abs(i) + " days ahead" : i + " days ago"}`,
+      });
+    }
+  }
+  return rangeOptions;
+}
+
+export { lastNumDays, createRangeOptions };
